37ff1ddcf5dd748f5ebd76cfae9c2f5302a13a9b
mishankov <mishankov@mail.com>
2022-01-18 13:34:12 +0300

    copy pzInsKey to clipboard working

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,6 +1,4 @@
-async function sleep(ms: number) {
-    return new Promise(r => setTimeout(r, ms));
-}
+    import { sleep, copyToClipboard } from "./utils"
 
 async function run() {
     let isBusyPrevious = false;
@@ -9,15 +7,9 @@ async function run() {
         let isBusy = el.getAttribute("data-state-busy-status") === "busy";
 
         if (isBusyPrevious && !isBusy) {
-            console.log("Now we are ready to do stuff");
-
             let ruleIframe = <HTMLIFrameElement> document.querySelector("#PEGA_TABBED0 > div.yui-content.contents.tabContent > div.iframe-wrapper[aria-hidden=false] > iframe");
             
-            console.log(ruleIframe);
-            
             if (ruleIframe !== null) {
-                // src="/prweb/app/test-cosmos_/8-wjN6771Z4s56SjWJe4WUkt6KfhDunu*/!TABTHREAD0?pyActivity=%40baseclass.doUIAction&action=openRuleSpecific&openHandle=RULE-OBJ-REPORT-DEFINITION%20BANK-LOAN-WORK-LOAN%20TST%20%2320220116T085439.861%20GMT&Format=harness&contentID=cf3a71e4-4395-9582-d62b-27617c4bc812&dynamicContainerID=de12ecd7-b2b3-4849-bff1-ee2797014513&tabIndex=2&portalThreadName=STANDARD&portalName=Developer&eventSrcSection=%40baseclass.pyFatListItems&pzHarnessID=HIDC1CD9EA8E995D1D5CB767538A9C9831A"
-                // src="/prweb/app/test-cosmos_/8-wjN6771Z4s56SjWJe4WUkt6KfhDunu*/!TABTHREAD0?pyActivity=%40baseclass.doUIAction&action=displayOnPage&renderAction=displayOnPage&harnessName=RuleForm&pzPrimaryPage=RH_1&contentID=cf3a71e4-4395-9582-d62b-27617c4bc812&dynamicContainerID=de12ecd7-b2b3-4849-bff1-ee2797014513&tabIndex=2&className=Rule-Obj-Report-Definition&label=Tst&key=RULE-OBJ-REPORT-DEFINITION%20BANK-LOAN-WORK-LOAN%20TST%20%2320220116T085439.861%20GMT&portalThreadName=STANDARD&portalName=Developer&pzHarnessID=HID676F4DED8FFEB9391EEAAF2A5283338D"
                 let ruleIframeSrc = ruleIframe.getAttribute("src");
 
                 let ruleKeyParameter = "&key="
@@ -26,14 +18,26 @@ async function run() {
                 }
                 
                 let ruleKey = ruleIframe.getAttribute("src").split(ruleKeyParameter)[1].split("&")[0].replaceAll("%20", " ").replaceAll("%23", "#");
-                console.log(ruleKey);
+                let copyRuleElementId = "pct_copy_" + ruleKey.replaceAll(" ", "_");
+                let ruleDocument = ruleIframe.contentWindow.document;
 
-                let ruleTable = ruleIframe.contentWindow.document.getElementById("harness-container");
-                let myDiv = ruleIframe.contentWindow.document
-                    .querySelectorAll("#harness-container > tbody > tr > td > div > div > span")[1]
-                    .querySelector("div > div > span > div > div > div > div > div > div > div > div > div > div > div > div > div > div");
+                if (ruleDocument.getElementById(copyRuleElementId) === null) {
+                    let ruleHeaderDiv = ruleDocument
+                        .querySelectorAll("#harness-container > tbody > tr > td > div > div > span")[1]
+                        .querySelector("div > div > span > div > div > div > div > div > div > div > div > div > div > div > div > div > div");
 
-                console.log(myDiv);
+                    let copyRuleElement = ruleDocument.createElement("i");
+                    copyRuleElement.setAttribute("class", "pi pi-copy");
+                    copyRuleElement.setAttribute("id", copyRuleElementId);
+                    copyRuleElement.setAttribute("rule-key", ruleKey);
+                    copyRuleElement.style.cursor = "pointer";
+
+                    copyRuleElement.addEventListener('click', event => {
+                        copyToClipboard(copyRuleElement.getAttribute("rule-key"));
+                    });
+
+                    ruleHeaderDiv.appendChild(copyRuleElement); 
+                }
             }
         }
 
---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+async function sleep(ms: number) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+function copyToClipboard(textContent: string) {
+    var target = document.createElement("textarea");
+    target.style.position = "fixed";
+    target.textContent = textContent;
+    document.body.appendChild(target);
+    
+    target.select();
+    document.execCommand("copy");
+    
+    document.body.removeChild(target);
+}
+
+export { sleep, copyToClipboard }
