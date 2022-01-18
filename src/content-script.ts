7356a53614d650092cb19a5e465d6d4c7ba3bb04
mishankov <mishankov@mail.com>
2022-01-18 22:10:15 +0300

    add tab close on middle button

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,46 +1,67 @@
 import { sleep, copyToClipboard } from "./utils"
 
+async function addCopyRuleKeyIcons() {
+    let ruleIframe = <HTMLIFrameElement> document.querySelector("#PEGA_TABBED0 > div.yui-content.contents.tabContent > div.iframe-wrapper[aria-hidden=false] > iframe");
+        
+    if (ruleIframe !== null) {
+        let ruleIframeSrc = ruleIframe.getAttribute("src");
+
+        if (ruleIframeSrc === undefined || ruleIframeSrc === "about:blank") return;
+
+        let ruleKeyParameter = "&key="
+        if ( ruleIframeSrc.includes("action=openRuleSpecific")) {
+            ruleKeyParameter = "&openHandle=";
+        }
+        
+        let ruleKey = ruleIframe.getAttribute("src").split(ruleKeyParameter)[1].split("&")[0].replaceAll("%20", " ").replaceAll("%23", "#");
+        let copyRuleElementId = "pct_copy_" + ruleKey.replaceAll(" ", "_");
+        let ruleDocument = ruleIframe.contentWindow.document;
+
+        if (ruleDocument.getElementById(copyRuleElementId) === null) {
+            let middleSpanList = ruleDocument.querySelectorAll("#harness-container > tbody > tr > td > div > div > span")
+            if (middleSpanList.length > 1) {
+                let ruleHeaderDiv = middleSpanList[1].querySelector("div > div > span > div > div > div > div > div > div > div > div > div > div > div > div > div > div");
+
+                if (ruleHeaderDiv !== null) {
+                    let copyRuleElement = ruleDocument.createElement("i");
+                    copyRuleElement.setAttribute("class", "pi pi-copy");
+                    copyRuleElement.setAttribute("id", copyRuleElementId);
+                    copyRuleElement.setAttribute("pct-rule-key", ruleKey);
+                    copyRuleElement.style.cursor = "pointer";
+
+                    copyRuleElement.addEventListener('click', event => {
+                        copyToClipboard(copyRuleElement.getAttribute("pct-rule-key"));
+                    });
+
+                    ruleHeaderDiv.appendChild(copyRuleElement);
+                }
+            }
+        }
+    }
+}
+
+async function addMiddleMouseTabClose() {
+    let tabs = document.querySelectorAll("div.dc-header ul[role=tablist] li[role=tab]");
+
+    for (let tab of tabs) {
+        if (tab.getAttribute("title") !== "Home" && tab.getAttribute("pct-middle-click-close") !== "true") {
+            tab.setAttribute("pct-middle-click-close", "true");
+            tab.addEventListener('auxclick', event => {
+                console.log(event);
+                if ((event as MouseEvent).which == 2) {
+                    (tab.querySelector('#close') as HTMLElement).click();
+                }
+            });
+        }
+    }
+}
+
 async function mainContentLoop() {
     while (true) {
         await sleep(1000);
 
-        let ruleIframe = <HTMLIFrameElement> document.querySelector("#PEGA_TABBED0 > div.yui-content.contents.tabContent > div.iframe-wrapper[aria-hidden=false] > iframe");
-        
-        if (ruleIframe !== null) {
-            let ruleIframeSrc = ruleIframe.getAttribute("src");
-
-            if (ruleIframeSrc === undefined || ruleIframeSrc === "about:blank") continue;
-
-            let ruleKeyParameter = "&key="
-            if ( ruleIframeSrc.includes("action=openRuleSpecific")) {
-                ruleKeyParameter = "&openHandle=";
-            }
-            
-            let ruleKey = ruleIframe.getAttribute("src").split(ruleKeyParameter)[1].split("&")[0].replaceAll("%20", " ").replaceAll("%23", "#");
-            let copyRuleElementId = "pct_copy_" + ruleKey.replaceAll(" ", "_");
-            let ruleDocument = ruleIframe.contentWindow.document;
-
-            if (ruleDocument.getElementById(copyRuleElementId) === null) {
-                let middleSpanList = ruleDocument.querySelectorAll("#harness-container > tbody > tr > td > div > div > span")
-                if (middleSpanList.length > 1) {
-                    let ruleHeaderDiv = middleSpanList[1].querySelector("div > div > span > div > div > div > div > div > div > div > div > div > div > div > div > div > div");
-
-                    if (ruleHeaderDiv !== null) {
-                        let copyRuleElement = ruleDocument.createElement("i");
-                        copyRuleElement.setAttribute("class", "pi pi-copy");
-                        copyRuleElement.setAttribute("id", copyRuleElementId);
-                        copyRuleElement.setAttribute("rule-key", ruleKey);
-                        copyRuleElement.style.cursor = "pointer";
-
-                        copyRuleElement.addEventListener('click', event => {
-                            copyToClipboard(copyRuleElement.getAttribute("rule-key"));
-                        });
-
-                        ruleHeaderDiv.appendChild(copyRuleElement);
-                    }
-                }
-            }
-        }
+        addCopyRuleKeyIcons();
+        addMiddleMouseTabClose();
     }
 }
 
