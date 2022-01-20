2bb237b2cb9ba87082c936cbfdee20a69d33e07a
mishankov <mishankov@mail.com>
2022-01-20 17:07:05 +0300

    log default config

--- src/background.ts
+++ src/background.ts
@@ -2,5 +2,8 @@ import { config } from "./common";
 
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ config });
+    chrome.storage.local.set({ config }, function() {
+        console.log("Default config is saved");
+        console.log(config)
+    });
 });
